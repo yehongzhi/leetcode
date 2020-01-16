0efb0365f0d48ea0df5833ebbd9cded29c79c85a
yehongzhi <yehongzhi@do1.com.cn>
2020-01-16 16:58:16 +0800

    done 1

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -8,13 +8,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (target - nums[j] == nums[i]) {
                     return new int[] { i, j };
                 }
             }
         }
-        return new int[]{};
+        return new int[] {};
     }
 }
 // @lc code=end
--- 2.两数相加.java
+++ 2.两数相加.java
@@ -5,22 +5,21 @@
  */
 
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+
+//   Definition for singly-linked list.
  class Solution {
-    // public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
-    // }
-
-    public static void main(String[] args) {
-        System.out.println("123");
+    
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
     }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        return null;
+    }
+
 }
 // @lc code=end
 
