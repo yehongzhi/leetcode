b487cc22509a0cba0a739a1b646844493ffc5125
yehongzhi <yehongzhi@do1.com.cn>
2020-01-18 09:33:14 +0800

    添加注释

--- 9.回文数.java
+++ 9.回文数.java
@@ -8,10 +8,16 @@
 class Solution {
     /**
      * 思路：转成字符串，然后反转过来。判断原来的字符串和反转后的字符串是否相等
-     * 结果：
+     * 
+     * 使用String拼接字符串的结果
      * 11509/11509 cases passed (13 ms)
      * Your runtime beats 25.51 % of java submissions
      * Your memory usage beats 33.85 % of java submissions (36.6 MB)
+     * 
+     * 使用StringBuilder拼接字符串，优化后：
+     * 11509/11509 cases passed (11 ms) 快了两毫秒
+     * Your runtime beats 51.09 % of java submissions
+     * Your memory usage beats 9.8 % of java submissions (37.1 MB)
      */
     public boolean isPalindrome(int x) {
         // 如果是负数，肯定不是回文数
