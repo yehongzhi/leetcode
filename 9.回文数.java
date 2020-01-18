924be69d587fad47e3b60b6dd2520109b8927883
yehongzhi <yehongzhi@do1.com.cn>
2020-01-18 09:32:34 +0800

    done 9

---
+++ 9.回文数.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=java
+ *
+ * [9] 回文数
+ */
+
+// @lc code=start
+class Solution {
+    /**
+     * 思路：转成字符串，然后反转过来。判断原来的字符串和反转后的字符串是否相等
+     * 结果：
+     * 11509/11509 cases passed (13 ms)
+     * Your runtime beats 25.51 % of java submissions
+     * Your memory usage beats 33.85 % of java submissions (36.6 MB)
+     */
+    public boolean isPalindrome(int x) {
+        // 如果是负数，肯定不是回文数
+        if (x < 0) {
+            return false;
+        }
+        // 输入的数字转换成字符串
+        String soureString = String.valueOf(Integer.valueOf(x));
+        // 转成字符数组
+        char[] chars = soureString.toCharArray();
+        // 遍历数组，反转过来
+        StringBuilder targetString = new StringBuilder();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            targetString.append(chars[i]);
+        }
+        // 判断是否是回文数
+        return targetString.toString().equals(soureString);
+    }
+}
+// @lc code=end
