f8a38f39cfcc3eae43c91f796f82a051f75a459a
java技术爱好者 <48724798+yehongzhi@users.noreply.github.com>
2020-08-07 15:58:47 +0800

    Create [101]对称二叉树.java

---
+++ [101]对称二叉树.java
@@ -0,0 +1,64 @@
+//
+// 
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。 
+//
+//     1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+// 
+//
+// 
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的: 
+//
+//     1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？ 
+// Related Topics 树 深度优先搜索 广度优先搜索
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return dfs(root.left, root.right);
+    }
+
+    private boolean dfs(TreeNode leftNode, TreeNode rightNode) {
+        if (leftNode == null && rightNode == null) {
+            return true;
+        }
+        if (leftNode == null || rightNode == null) {
+            return false;
+        }
+        if (leftNode.val != rightNode.val) {
+            return false;
+        }
+        //左节点的左子节点与右节点的右子节点相等 并且 左节点的右子节点与右节点的左子节点相等
+        return dfs(leftNode.left, rightNode.right) && dfs(leftNode.right, rightNode.left);
+    }
+}
