80cd874a2a218420aafd08bd625a61365a433186
java技术爱好者 <48724798+yehongzhi@users.noreply.github.com>
2020-08-07 15:57:09 +0800

    Create [53]最大子序和.java

---
+++ [53]最大子序和.java
@@ -0,0 +1,30 @@
+//
+// 示例: 
+//
+// 输入: [-2,1,-3,4,-1,2,1,-5,4],
+//输出: 6
+//解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+// 
+//
+// 进阶: 
+//
+// 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。 
+// Related Topics 数组 分治算法 动态规划
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int res = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int num : nums) {
+            if (sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            res = Math.max(sum, res);
+        }
+        return res;
+    }
+}
