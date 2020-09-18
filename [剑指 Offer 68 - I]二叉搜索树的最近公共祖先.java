877eeb0d60aec50e907eeff85f1d00b869926ecd
java技术爱好者 <48724798+yehongzhi@users.noreply.github.com>
2020-09-18 14:49:38 +0800

    Create [剑指 Offer 68 - I]二叉搜索树的最近公共祖先.java

---
+++ [剑指 Offer 68 - I]二叉搜索树的最近公共祖先.java
@@ -0,0 +1,16 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode leftTreeNode = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightTreeNode = lowestCommonAncestor(root.right, p, q);
+        if (leftTreeNode == null) {
+            return rightTreeNode;
+        }
+        if (rightTreeNode == null) {
+            return leftTreeNode;
+        }
+        return root;
+    }
+}
