6a8ec6b39d1e6ec12dffb09594943bf3a70f1673
java技术爱好者 <48724798+yehongzhi@users.noreply.github.com>
2020-09-18 14:36:05 +0800

    Create [剑指 Offer 10- I]斐波那契数列.java

---
+++ [剑指 Offer 10- I]斐波那契数列.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 2];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i < dp.length - 1; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+        return dp[n];
+    }
+}
