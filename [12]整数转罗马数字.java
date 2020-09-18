be63bf7ead7f2c4d77445466316787b0fc16b07c
java技术爱好者 <48724798+yehongzhi@users.noreply.github.com>
2020-09-18 10:18:59 +0800

    Create [12]整数转罗马数字.java

---
+++ [12]整数转罗马数字.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    private static int[] values = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+    private static String[] symbols = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (values[i] <= num) {
+                num -= values[i];
+                sb.append(symbols[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
