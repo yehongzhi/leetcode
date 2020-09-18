ace77beb54683a6b3e8d8a780f50684d97a84efd
java技术爱好者 <48724798+yehongzhi@users.noreply.github.com>
2020-09-18 10:32:58 +0800

    Create [112]路径总和.java

---
+++ [112]路径总和.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return sum - root.val == 0;
+        } else {
+            return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        }
+    }
+}
