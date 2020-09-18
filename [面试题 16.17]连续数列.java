ae0968a0008d3bbbccfe366b3d7125648c1d8302
java技术爱好者 <48724798+yehongzhi@users.noreply.github.com>
2020-09-18 15:02:23 +0800

    Create [面试题 16.17]连续数列.java

---
+++ [面试题 16.17]连续数列.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int maxSubArray(int[] nums) {
+        int res = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int num : nums) {
+            if (sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            res = Math.max(res, sum);
+        }
+        return res;
+    }
+}
