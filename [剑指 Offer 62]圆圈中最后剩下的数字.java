41048350fa3c9910877a39bc54cbf80542e4a135
java技术爱好者 <48724798+yehongzhi@users.noreply.github.com>
2020-09-18 14:48:42 +0800

    Create [剑指 Offer 62]圆圈中最后剩下的数字.java

---
+++ [剑指 Offer 62]圆圈中最后剩下的数字.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int lastRemaining(int n, int m) {
+        List<Integer> list = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        int index = 0;
+        while (n > 1) {
+            index = (index + m - 1) % n;
+            list.remove(index);
+            n--;
+        }
+        return list.get(0);
+    }
+}
