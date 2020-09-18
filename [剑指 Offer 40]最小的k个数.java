e77975225add5ccdb34ff7d4019436e37fffca56
java技术爱好者 <48724798+yehongzhi@users.noreply.github.com>
2020-09-18 14:42:47 +0800

    Create [剑指 Offer 40]最小的k个数.java

---
+++ [剑指 Offer 40]最小的k个数.java
@@ -0,0 +1,9 @@
+class Solution {
+
+    public int[] getLeastNumbers(int[] arr, int k) {
+        Arrays.sort(arr);
+        int[] res = new int[k];
+        System.arraycopy(arr, 0, res, 0, res.length);
+        return res;
+    }
+}
