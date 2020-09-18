9e23c170bb86edd97eaccf611ad9828ebdbf196c
java技术爱好者 <48724798+yehongzhi@users.noreply.github.com>
2020-09-18 11:40:51 +0800

    Create [1290]二进制链表转整数.java

---
+++ [1290]二进制链表转整数.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+        while (head != null) {
+            sb.append(head.val);
+            head = head.next;
+        }
+        return Integer.parseInt(sb.toString(), 2);
+    }
+}
