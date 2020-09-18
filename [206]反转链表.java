93947e4d13c20737ce76ef7d938970659f8a2e83
java技术爱好者 <48724798+yehongzhi@users.noreply.github.com>
2020-09-18 10:42:46 +0800

    Create [206]反转链表.java

---
+++ [206]反转链表.java
@@ -0,0 +1,13 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = temp;
+        }
+        return pre;
+    }
+}
