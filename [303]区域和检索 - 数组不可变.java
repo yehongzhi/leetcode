116b65afa8ad65eb6e52249216e74f2a9db41436
java技术爱好者 <48724798+yehongzhi@users.noreply.github.com>
2020-09-18 10:52:58 +0800

    Create [303]区域和检索 - 数组不可变.java

---
+++ [303]区域和检索 - 数组不可变.java
@@ -0,0 +1,15 @@
+class NumArray {
+
+    private int[] sums;
+
+    public NumArray(int[] nums) {
+        this.sums = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+}
