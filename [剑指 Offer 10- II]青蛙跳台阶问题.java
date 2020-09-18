27fa95fefe3ffff048727b163e3b68b2bad41510
java技术爱好者 <48724798+yehongzhi@users.noreply.github.com>
2020-09-18 14:36:37 +0800

    Create [剑指 Offer 10- II]青蛙跳台阶问题.java

---
+++ [剑指 Offer 10- II]青蛙跳台阶问题.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public int numWays(int n) {
+        if (n < 2) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= dp.length - 1; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+        return dp[n];
+    }
+}
